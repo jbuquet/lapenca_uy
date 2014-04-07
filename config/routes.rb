e67d4f2c822a63db4f663d8c93435a563d88e984
Javier Buquet <jbuquet@gmail.com>
2014-04-06 21:49:17 -0300

    Add Teams, Arenas, Matches and Forecasts controllers and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 LapencaUy::Application.routes.draw do
   devise_for :members
 
+  resources :teams
+  resources :arenas
+  resources :matches
+  resources :forecasts
+
   get "static_pages/home"
   match '/home', to: 'static_pages#home', via: 'get'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407003754) do
+ActiveRecord::Schema.define(version: 20140407010234) do
 
   create_table "arenas", force: true do |t|
     t.string   "name"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20140407003754) do
     t.string   "coach"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "group"
   end
 
 end
