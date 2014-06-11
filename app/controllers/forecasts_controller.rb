ee982f3172684f794df7f577eb731de5df4230e3
Javier Buquet <jbuquet@gmail.com>
2014-06-11 01:41:49 -0300

    Disable forecasts update after predefined deadline

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -4,18 +4,29 @@ class ForecastsController < ApplicationController
   # GET /forecasts
   # GET /forecasts.json
   def index
-    @matches = Match.all.group_by(&:group).sort_by { |group, _| group }
+    if Date.parse('2014-06-11') < Time.zone.now.to_date
+      @matches = Match.all.group_by(&:group).sort_by { |group, _| group }
+      render :index_no_stage
+    else
+      @matches = Match.all.group_by(&:group).sort_by { |group, _| group }
+      render :index_groups_stage
+    end
   end
 
   # POST /forecasts
   # POST /forecasts.json
   def create
-    params.require(:forecast).each do |match_id, attrs|
-      user_forecast = Forecast.find_or_create_by(member_id: current_member.id, match_id: match_id)
-      user_forecast.update_attributes(attrs)
+    if Date.parse('2014-06-11') < Time.zone.now.to_date
+      flash[:alert] = 'Ya no se pueden actualizar los pronosticos'
+    else
+      params.require(:forecast).each do |match_id, attrs|
+        user_forecast = Forecast.find_or_create_by(member_id: current_member.id, match_id: match_id)
+        user_forecast.update_attributes(attrs)
+      end
+
+      flash[:notice] = 'Pronostico actualizado correctamente'
     end
 
-    flash[:notice] = 'Pronostico actualizado correctamente'
     redirect_to forecasts_path
   end
 
